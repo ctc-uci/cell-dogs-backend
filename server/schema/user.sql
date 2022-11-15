1e620449d1c5e08f165269461f0dd1906079e3e0
liannejl <liannejl@uci.edu>
2022-11-14 17:35:58 -0800

    created user table

---
+++ server/schema/user.sql
@@ -0,0 +1,7 @@
+create table IF NOT EXISTS User(
+    id varchar(50) unique not null;
+    email varchar(50) primary key not null;
+    firstName varchar(50) not null;
+    lastName varchar(50) not null;
+    facility int not null;
+);
