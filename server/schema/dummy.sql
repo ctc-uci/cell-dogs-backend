57e4f13aca64049c6dd6b3ad3f8621a66d018dc9
liannejl <liannejl@uci.edu>
2022-11-14 18:36:31 -0800

    added dummy data

---
+++ server/schema/dummy.sql
@@ -0,0 +1,8 @@
+insert into User (id, email, firstName, lastName, facility)
+values ("lsdlj3o23923", "cds@uci.edu", "Cell", "Dogs", 4);
+
+insert into User (id, email, firstName, lastName, facility)
+values ("qwerwr23", "liptonja@uci.edu", "Josh", "Lipton", 11);
+
+insert into User (id, email, firstName, lastName, facility)
+values ("fdsj2j32k23kljlkssdsdf", "ctc@gmail.com", "Commit", "Change", 2);
