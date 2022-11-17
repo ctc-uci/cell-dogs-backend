5a94f97c759e1dd6596878930e066485a86e3672
liannejl <liannejl@uci.edu>
2022-11-16 17:40:08 -0800

    edited user.js with new alterend table attr names

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const pool = require('../server/db');
 
 user.get('/user', async (req, res) => {
   try {
-    const allUserInfo = await pool.query('select * from User');
+    const allUserInfo = await pool.query('select * from public.user');
     res.send(allUserInfo.rows);
   } catch (err) {
     res.status(400).send(err.message);
@@ -15,7 +15,7 @@ user.get('/user', async (req, res) => {
 user.get('/user/:email', async (req, res) => {
   try {
     const { email } = req.params;
-    const userInfo = await pool.query(`select * from User where email = ${email}`);
+    const userInfo = await pool.query(`select * from public.user where email = '${email}'`);
     res.send(userInfo.rows);
   } catch (err) {
     res.status(400).send(err.message);
@@ -26,7 +26,7 @@ user.post('/user', async (req, res) => {
   try {
     const { id, email, firstName, lastName, facility } = req.body;
     const newUser = await pool.query(
-      `insert into User (id, email, firstName, lastName, facility) values (${id}, ${email}, ${firstName}, ${lastName}, ${facility}) returning *`,
+      `insert into public.user (id, email, first_name, last_name, facility) values ('${id}', '${email}', '${firstName}', '${lastName}', '${facility}') returning *`,
     );
     res.send(newUser.rows);
   } catch (err) {
@@ -37,7 +37,7 @@ user.post('/user', async (req, res) => {
 user.delete('/user/:email', async (req, res) => {
   try {
     const { email } = req.params;
-    await pool.query(`delete from User where email = ${email}}`);
+    await pool.query(`delete from public.user where email = '${email}'}`);
     res.send(`User with email ${email} was deleted.`);
   } catch (err) {
     res.status(400).send(err.message);
@@ -61,23 +61,25 @@ user.put('/user/:email', async (req, res) => {
     const { facility } = req.body.facility;
     const { newEmail } = req.body.email;
     if (id !== '') {
-      await pool.query(`update User set id = ${id} where email = ${email}`);
+      await pool.query(`update public.user set id = '${id}' where email = '${email}'`);
     }
 
     if (firstName !== '') {
-      await pool.query(`update User set firstName = ${firstName} where email = ${email}`);
+      await pool.query(
+        `update public.user set firstName = '${firstName}' where email = '${email}'`,
+      );
     }
 
     if (lastName !== '') {
-      await pool.query(`update User set lastName = ${lastName} where email = ${email}`);
+      await pool.query(`update public.user set lastName = '${lastName}' where email = '${email}'`);
     }
 
     if (facility !== '') {
-      await pool.query(`update User set facility = ${facility} where email = ${email}`);
+      await pool.query(`update public.user set facility = '${facility}' where email = '${email}'`);
     }
 
     if (newEmail !== '') {
-      await pool.query(`update User set email = ${newEmail} where email = ${email}`);
+      await pool.query(`update public.user set email = '${newEmail}' where email = '${email}'`);
     }
   } catch (err) {
     res.status(400).send(err.message);
