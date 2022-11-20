097a13bacc7eeb4bffe4119569bbdb8d940c5cc7
“Michelle <itsmichellelin@gmail.com>
2022-11-19 16:19:09 -0800

    CRUD routes for facility table

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+const facilityRouter = require('./routes/facility');
+
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,6 +15,8 @@ app.use(
     origin: `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`,
   }),
 );
+
+app.use('/facility', facilityRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ routes/facility.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const { db } = require('../server/db');
+
+const facilityRouter = express.Router();
+
+// GET all facilities
+facilityRouter.get('/', async (req, res) => {
+  try {
+    const allFacilities = await db.query('SELECT * FROM facility');
+    res.status(200).send(allFacilities);
+    console.log(allFacilities);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// GET facility by id
+facilityRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const facility = await db.query('SELECT * FROM facility WHERE facility_id = $(id)', { id });
+    res.status(200).send(facility);
+    console.log(facility);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// CREATE a new facility
+facilityRouter.post('/', async (req, res) => {
+  try {
+    console.log(req);
+    const { name, addressLine, city, state, zip, description } = req.body;
+    const newFacility = await db.query(
+      'INSERT INTO facility (name, address_line, city, state, zip, description) VALUES ($(name), $(addressLine), $(city), $(state), $(zip), $(description)) RETURNING *',
+      { name, addressLine, city, state, zip, description },
+    );
+    res.status(200).send(newFacility);
+    console.log(newFacility);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// UPDATE facility rows
+facilityRouter.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, addressLine, city, state, zip, description } = req.body;
+    const updatedFacility = await db.query(
+      `UPDATE facility
+      SET name = $(name), address_line = $(addressLine), city = $(city), state = $(state), zip = $(zip), description = $(description)
+      WHERE facility_id = $(id)
+      RETURNING *;`,
+      { id, name, addressLine, city, state, zip, description },
+    );
+    res.status(200).send(updatedFacility);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// DELETE a facility
+facilityRouter.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await db.query('DELETE from facility WHERE facility_id = $(id)', { id });
+    res.status(200).send('Deleted facility');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = facilityRouter;
