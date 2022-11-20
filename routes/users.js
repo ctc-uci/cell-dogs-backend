2d4c5a786780094eb546e46df59bece60410e82f
liannejl <liannejl@uci.edu>
2022-11-20 00:55:28 -0800

    tested confirmed that post, delete, get work, cannot debug put

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,11 @@ const express = require('express');
 const user = express.Router(); // This should be an express.Router instance;
 const { pool } = require('../server/db');
 
+user.use(express.json());
+user.use(express.urlencoded());
+
+const { isNumeric } = require('../common/utils');
+
 user.get('/', async (req, res) => {
   try {
     const allUserInfo = await pool.query('select * from public.user');
@@ -24,9 +29,10 @@ user.get('/:email', async (req, res) => {
 
 user.post('/', async (req, res) => {
   try {
-    const { id, email, firstName, lastName, facility } = req.body;
+    const { id, email, firstName, lastName, facility } = req.query;
+
     const newUser = await pool.query(
-      `insert into public.user (id, email, first_name, last_name, facility) values ('${id}', '${email}', '${firstName}', '${lastName}', '${facility}') returning *`,
+      `insert into public.user (id, email, first_name, last_name, facility) values ('${id}', '${email}', '${firstName}', '${lastName}', ${facility}) returning *`,
     );
     res.send(newUser.rows);
   } catch (err) {
@@ -37,7 +43,7 @@ user.post('/', async (req, res) => {
 user.delete('/:email', async (req, res) => {
   try {
     const { email } = req.params;
-    await pool.query(`delete from public.user where email = '${email}'}`);
+    await pool.query(`delete from public.user where email = '${email}'`);
     res.send(`User with email ${email} was deleted.`);
   } catch (err) {
     res.status(400).send(err.message);
@@ -48,7 +54,15 @@ user.put('/:email', async (req, res) => {
   try {
     const { email } = req.params;
 
-    const { id, firstName, lastName, facility, newEmail } = req.body;
+    // const { id } = req.body.id;
+    // const { firstName } = req.body.first_name;
+    // const { lastName } = req.body.last_name;
+    // const { newEmail } = req.body.email;
+    // const { facility } = req.body.facility;
+
+    const { id, newEmail, firstName, lastName, facility } = req.body;
+
+    // const { id, firstName, lastName, facility, newEmail } = req.query;
 
     if (id !== null) {
       await pool.query(`update public.user set id = '${id}' where email = '${email}'`);
@@ -56,16 +70,16 @@ user.put('/:email', async (req, res) => {
 
     if (firstName !== null) {
       await pool.query(
-        `update public.user set firstName = '${firstName}' where email = '${email}'`,
+        `update public.user set first_name = '${firstName}' where email = '${email}'`,
       );
     }
 
     if (lastName !== null) {
-      await pool.query(`update public.user set lastName = '${lastName}' where email = '${email}'`);
+      await pool.query(`update public.user set last_name = '${lastName}' where email = '${email}'`);
     }
 
-    if (facility !== null) {
-      await pool.query(`update public.user set facility = '${facility}' where email = '${email}'`);
+    if (facility !== null && isNumeric(facility)) {
+      await pool.query(`update public.user set facility = ${facility} where email = '${email}'`);
     }
 
     if (newEmail !== null) {
