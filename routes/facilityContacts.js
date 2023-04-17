ed23c5086698991b73f5f33b89966b4e36347986
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2023-04-16 21:25:33 -0700

    Created routes for facility contacts (#18)
    * added to .gitignore
    
    * created user table
    
    * user.js in progress
    
    * finished writing user.js
    
    * added dummy data
    
    * resolved pg not found error
    
    * added user table and dummy data
    
    * edited user.js with new alterend table attr names
    
    * tested confirmed that post, delete, get work, cannot debug put
    
    * finish put route
    
    * finish put route
    
    * changed to pg promise
    
    * got post and get without id to work
    
    * fix delete and put routes
    
    * fixed put and delete requests
    
    ---------
    
    Co-authored-by: liannejl <liannejl@uci.edu>
    Co-authored-by: joshualipton <jalipton11@gmail.com>
    Co-authored-by: Madhu2244 <madhuxsharma@gmail.com>
    Co-authored-by: “Michelle <itsmichellelin@gmail.com>

---
+++ routes/facilityContacts.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const { db } = require('../server/db');
+
+const facilityContacts = express();
+
+// GET all facility contacts
+facilityContacts.get('/', async (req, res) => {
+  try {
+    const allContacts = await db.query('SELECT * FROM facility_contacts');
+    res.status(200).send(allContacts);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// GET facility contact by id
+facilityContacts.get('/:facilityId', async (req, res) => {
+  try {
+    const { facilityId } = req.params;
+    const contact = await db.query(
+      `SELECT * FROM public.facility_contacts WHERE facility_id = $(facilityId)`,
+      {
+        facilityId,
+      },
+    );
+    return res.status(200).send(contact);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
+// Create new facility contact
+facilityContacts.post('/', async (req, res) => {
+  try {
+    const { facilityId, name, title, phoneNumber, emailAddress } = req.body;
+    const allContacts = await db.query(
+      'INSERT INTO public.facility_contacts (facility_id, name, title, phone_number, email_address) VALUES ($(facilityId), $(name), $(title), $(phoneNumber), $(emailAddress)) RETURNING *',
+      {
+        facilityId,
+        name,
+        title,
+        phoneNumber,
+        emailAddress,
+      },
+    );
+    res.status(200).send(allContacts);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// UPDATE facility contact row
+facilityContacts.put('/:facilityId', async (req, res) => {
+  try {
+    const { facilityId } = req.params;
+    const { name, title, phoneNumber, emailAddress } = req.body;
+    const updatedFacilityContact = await db.query(
+      `UPDATE facility_contacts SET
+      facility_id = $(facilityId)
+      ${name ? `, name = $(name) ` : ''}
+      ${title ? `, title = $(title) ` : ''}
+      ${phoneNumber ? `, phone_number = $(phoneNumber) ` : ''}
+      ${emailAddress ? `, email_address = $(emailAddress) ` : ''}
+      WHERE facility_id = $(facilityId)
+      RETURNING *;`,
+      { facilityId, name, title, phoneNumber, emailAddress },
+    );
+    return res.status(200).send(updatedFacilityContact);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
+
+// DELETE a facility contact
+facilityContacts.delete('/:facilityId', async (req, res) => {
+  try {
+    const { facilityId } = req.params;
+    await db.query('DELETE from facility_contacts WHERE facility_id = $(facilityId)', {
+      facilityId,
+    });
+    res.status(200).send('Deleted facility');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+module.exports = facilityContacts;
